735e4edec4882728a673ca5a378b2ed517a3eccc
high-moctane <high.moctane@moctane.com>
2024-10-05 19:56:02 +0900

    Added Decoder.ExpectNull()

--- mocjson_test.go
+++ mocjson_test.go
@@ -7,6 +7,67 @@ import (
 	"github.com/high-moctane/mocjson-go"
 )
 
+func TestDecoder_ExpectNull(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name    string
+		input   []byte
+		wantErr bool
+	}{
+		{
+			name:    "null",
+			input:   []byte("null"),
+			wantErr: false,
+		},
+		{
+			name:    "null and some extra characters",
+			input:   []byte("nullabc"),
+			wantErr: false,
+		},
+		{
+			name:    "null: too short",
+			input:   []byte("nul"),
+			wantErr: true,
+		},
+		{
+			name:    "invalid: empty",
+			input:   []byte(""),
+			wantErr: true,
+		},
+		{
+			name:    "invalid",
+			input:   []byte("invalid"),
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			var dec mocjson.Decoder
+
+			r := bytes.NewReader(tt.input)
+
+			if err := dec.ExpectNull(r); (err != nil) != tt.wantErr {
+				t.Errorf("UnmarshalNull() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func BenchmarkDecoder_ExpectNull(b *testing.B) {
+	var dec mocjson.Decoder
+	r := bytes.NewReader([]byte("null"))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r.Seek(0, 0)
+		_ = dec.ExpectNull(r)
+	}
+}
+
 func TestDecoder_ExpectBool(t *testing.T) {
 	t.Parallel()
 
