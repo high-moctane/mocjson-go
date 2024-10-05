42c5f3b32878211addfb5066dd38afcff7e1343b
high-moctane <high.moctane@moctane.com>
2024-10-05 19:56:02 +0900

    Added Decoder.ExpectNull()

--- mocjson.go
+++ mocjson.go
@@ -9,6 +9,16 @@ type Decoder struct {
 	buf [2 << 10]byte
 }
 
+func (d *Decoder) ExpectNull(r io.Reader) error {
+	if _, err := r.Read(d.buf[:4]); err != nil {
+		return fmt.Errorf("read error: %v", err)
+	}
+	if d.buf[0] != 'n' || d.buf[1] != 'u' || d.buf[2] != 'l' || d.buf[3] != 'l' {
+		return fmt.Errorf("invalid null value")
+	}
+	return nil
+}
+
 func (d *Decoder) ExpectBool(r io.Reader) (bool, error) {
 	if _, err := r.Read(d.buf[:1]); err != nil {
 		return false, fmt.Errorf("read error: %v", err)
