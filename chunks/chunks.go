dfcbdfeec58be7480723ac6b1f12686e37352227
high-moctane <high.moctane@moctane.com>
2024-11-02 23:08:53 +0900

    Added Reader.chunks

--- chunks/chunks.go
+++ chunks/chunks.go
@@ -2,8 +2,13 @@ package chunks
 
 import "io"
 
+const (
+	chunkLen = 8
+)
+
 type Reader struct {
-	r io.Reader
+	r      io.Reader
+	chunks [chunkLen]uint64
 }
 
 func NewReader(r io.Reader) *Reader {
