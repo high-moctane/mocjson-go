3a26826cfabea8ef729fd10b47820cd7911d8d65
high-moctane <high.moctane@moctane.com>
2024-11-20 23:28:25 +0900

    Added TestScanner_DigitLen()

--- mocjson_test.go
+++ mocjson_test.go
@@ -86,3 +86,60 @@ func BenchmarkScanner_WhiteSpaceLen(b *testing.B) {
 		sc.WhiteSpaceLen()
 	}
 }
+
+func TestScanner_DigitLen(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+		b    []byte
+		want int
+	}{
+		{
+			name: "digit only",
+			b:    []byte("1234567890"),
+			want: 10,
+		},
+		{
+			name: "digit and ascii",
+			b:    []byte("1234567890a"),
+			want: 10,
+		},
+		{
+			name: "json only",
+			b:    []byte("{\"key\": \"value\"}"),
+			want: 0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			r := bytes.NewReader(tt.b)
+			sc := NewScanner(r)
+
+			if sc.Load() == false {
+				t.Errorf("failed to load")
+			}
+
+			got := sc.DigitLen()
+			if got != tt.want {
+				t.Errorf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkScanner_DigitLen(b *testing.B) {
+	r := bytes.NewReader([]byte(strings.Repeat("1234567890", 100)[:100]))
+	sc := NewScanner(r)
+
+	if sc.Load() == false {
+		b.Errorf("failed to load")
+	}
+
+	for range b.N {
+		sc.DigitLen()
+	}
+}
