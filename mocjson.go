13f22b24c661c4ed97fbbdf07cf0a0fce18b9f99
high-moctane <high.moctane@moctane.com>
2024-12-15 23:41:42 +0900

    Added opCodeScannerLoad

--- mocjson.go
+++ mocjson.go
@@ -6,10 +6,12 @@ type opCode int
 
 const (
 	opCodeUndefined opCode = iota
+	opCodeScannerLoad
 )
 
 type data struct {
 	ops []opCode
+	sc  scanner
 }
 
 func (d *data) popOp() opCode {
@@ -18,11 +20,72 @@ func (d *data) popOp() opCode {
 	return op
 }
 
-func parse(r io.Reader, d *data) error {
+const (
+	bufSize = 64
+)
+
+type scanner struct {
+	buf       [bufSize]byte
+	rawcur    int
+	rawbufend int
+	err       error
+	r         io.Reader
+}
+
+func (*scanner) calcCur(n int) int {
+	return n % bufSize
+}
+
+func (sc *scanner) cur() int {
+	return sc.calcCur(sc.rawcur)
+}
+
+func (sc *scanner) bufend() int {
+	return sc.calcCur(sc.rawbufend)
+}
+
+func (sc *scanner) readBufFrom(from int) {
+	var n int
+	n, sc.err = sc.r.Read(sc.buf[from:])
+	sc.rawbufend = from + n
+}
+
+func (sc *scanner) readBufTo(to int) {
+	var n int
+	n, sc.err = sc.r.Read(sc.buf[:to])
+	sc.rawbufend = n
+}
+
+func (sc *scanner) readBufFromTo(from, to int) {
+	var n int
+	n, sc.err = sc.r.Read(sc.buf[from:to])
+	sc.rawbufend = from + n
+}
+
+func parse(d *data) error {
+loop:
 	for len(d.ops) > 0 {
 		op := d.popOp()
 
 		switch op {
+		case opCodeScannerLoad:
+			if d.sc.err != nil {
+				continue loop
+			}
+			cur := d.sc.cur()
+			bufend := d.sc.bufend()
+			switch {
+			case cur < bufend:
+				d.sc.readBufFrom(bufend)
+				if d.sc.err != nil {
+					continue loop
+				}
+				d.sc.readBufTo(cur)
+
+			case bufend < cur:
+				d.sc.readBufFromTo(bufend, cur)
+			}
+
 		default:
 			panic("invalid op code")
 		}
