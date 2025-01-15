cf4f449a8a31dec1ac8faba3c47013d7b88f5e80
high-moctane <high.moctane@moctane.com>
2025-01-16 00:55:46 +0900

    Added TestParser_ParseValue()

--- mocjson_test.go
+++ mocjson_test.go
@@ -1533,6 +1533,71 @@ func BenchmarkLexer_ExpectString(b *testing.B) {
 	}
 }
 
+func TestParser_ParseValue(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name    string
+		b       []byte
+		want    any
+		wantErr bool
+	}{
+		{
+			name: "array",
+			b:    []byte(`["value1","value2"]`),
+			want: []any{"value1", "value2"},
+		},
+		{
+			name: "object",
+			b:    []byte(`{"key1":"value1","key2":"value2"}`),
+			want: map[string]any{"key1": "value1", "key2": "value2"},
+		},
+		{
+			name: "null",
+			b:    []byte("null"),
+			want: nil,
+		},
+		{
+			name: "bool",
+			b:    []byte("true"),
+			want: true,
+		},
+		{
+			name: "number",
+			b:    []byte("1.0"),
+			want: 1.0,
+		},
+		{
+			name: "string",
+			b:    []byte(`"hello"`),
+			want: "hello",
+		},
+		{
+			name:    "invalid",
+			b:       []byte(""),
+			want:    nil,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			r := bytes.NewReader(tt.b)
+			pa := NewParser(r)
+
+			got, err := pa.ParseValue()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("gotErr %v, wantErr %v", err != nil, tt.wantErr)
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestParser_ParseArray(t *testing.T) {
 	t.Parallel()
 
