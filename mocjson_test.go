f97d3923a68444b7086e0586307bfb0d8a6ea772
high-moctane <high.moctane@moctane.com>
2025-01-18 21:05:26 +0900

    Improve TestLexer_NextTokenType()

--- mocjson_test.go
+++ mocjson_test.go
@@ -794,6 +794,18 @@ func TestLexer_NextTokenType(t *testing.T) {
 				t.Errorf("got %v, want %v", got, tt.want)
 			}
 		})
+
+		t.Run(tt.name+"; with long whitespaces", func(t *testing.T) {
+			t.Parallel()
+
+			r := bytes.NewReader(append(bytes.Repeat([]byte(" \t\r\n"), ScannerBufSize), tt.b...))
+			lx := NewLexer(r)
+
+			got := lx.NextTokenType()
+			if got != tt.want {
+				t.Errorf("got %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
