c773e268ff4adf0528c148ba46ea2fd9f720c4f2
high-moctane <high.moctane@moctane.com>
2025-01-18 21:10:22 +0900

    Improve TestLexer_ExpectBeginObject()

--- mocjson_test.go
+++ mocjson_test.go
@@ -1085,6 +1085,18 @@ func TestLexer_ExpectBeginObject(t *testing.T) {
 				t.Errorf("got %v, want %v", got, tt.want)
 			}
 		})
+
+		t.Run(tt.name+"; with long whitespaces", func(t *testing.T) {
+			t.Parallel()
+
+			r := bytes.NewReader(append(bytes.Repeat([]byte(" \t\r\n"), ScannerBufSize), tt.b...))
+			lx := NewLexer(r)
+
+			got := lx.ExpectBeginObject()
+			if got != tt.want {
+				t.Errorf("got %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
